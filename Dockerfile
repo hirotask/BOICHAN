04a3e118448d79793e1961c00d5d70a1fb6a707c
hirotask <hirotaskhashi@gmail.com>
2023-07-11 11:42:48 +0900

    新たなライブラリを追加

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,9 @@
 FROM node:lts-bullseye
 WORKDIR /app
 
+RUN apt-get -y update\
+    && apt-get -y upgrade\
+    && apt-get install -y ffmpeg
+
 COPY package*.json ./
 RUN npm install
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 //必要なパッケージをインポートする
-import { GatewayIntentBits, Client, Partials, Message, Snowflake, User } from 'discord.js'
+import { GatewayIntentBits, Client, Partials, Message, Snowflake, Embed, Attachment, AttachmentBuilder } from 'discord.js'
 import { joinVoiceChannel, VoiceConnection, createAudioPlayer, EndBehaviorType, VoiceReceiver, AudioReceiveStream, createAudioResource, StreamType } from '@discordjs/voice'
-import { OpusEncoder } from '@discordjs/opus'
 import dotenv from 'dotenv'
-import * as WavEncoder from 'wav-encoder'
-import { writeFileSync } from 'fs'
-import { PassThrough } from 'stream'
-import * as Prism from 'prism-media'
+import { writeFileSync, createWriteStream, unlinkSync } from 'fs'
+import { VoiceRecorder } from '@kirdock/discordjs-voice-recorder'
 
 //.envファイルを読み込む
 dotenv.config()
@@ -38,6 +35,8 @@ const SILENCE_BUFFER = Buffer.from([0xf8, 0xff, 0xfe]);
 const start_commands = ["議事録取って", "議事録開始", "!start"];
 const stop_commands = ["議事録とめて", "議事録終了", "!stop"];
 
+const voiceRecorder = new VoiceRecorder();
+
 //!timeと入力すると現在時刻を返信するように
 client.on('messageCreate', async (message: Message) => {
     if (message.author.bot) return
@@ -60,77 +59,8 @@ client.on('messageCreate', async (message: Message) => {
             //プレイヤーを音声接続オブジェクトに接続する
             connection.subscribe(player);
 
-            const receiver = connection.receiver;
-
-            receiver.speaking.on("start", (userId) => {
-                // const audioStream = receiver.subscribe(userId, {
-                //     end: {
-                //         behavior: EndBehaviorType.AfterSilence,
-                //         duration: 10
-                //     }
-                // });
-
-                const opusStream = receiver.subscribe(userId, {
-                    end: {
-                        behavior: EndBehaviorType.AfterSilence,
-                        duration: 100
-                    }
-                })
-                message.channel.send('録音を開始します')
-
-                const rawStream = new PassThrough()
-
-                opusStream.pipe(new Prism.opus.Decoder({ rate: 48000, channels: 2, frameSize: 960 })).pipe(rawStream)
-
-                // const leftOpusStream: Promise<Buffer>[] = [];
-                // const rightOpusStream: Promise<Buffer>[] = [];
-
-                // let isLeftChannel = true; // 初めは左チャンネル
-
-                // opusStream.on('data', (chunk) => {
-
-                //     const decodedChunk = decodeOpus(chunk);
-
-                //     if (isLeftChannel) {
-                //         leftOpusStream.push(decodedChunk);
-                //     } else {
-                //         rightOpusStream.push(decodedChunk);
-                //     }
-
-                //     isLeftChannel = !isLeftChannel; // チャンネルを切り替える
-                // });
-
-                opusStream.on('end', async () => {
-                    console.log(`Stream from user ${userId} has ended`);
-
-                    const resource = createAudioResource(rawStream, {
-                        inputType: StreamType.Raw
-                    })
-                    player.play(resource)
-
-                    // const leftPcmDataArray = await Promise.all(leftOpusStream);
-                    // const leftConcatenatedBuffer = Buffer.concat(leftPcmDataArray);
-                    // const rightPcmDataArray = await Promise.all(rightOpusStream);
-                    // const rightConcatenatedBuffer = Buffer.concat(rightPcmDataArray);
-
-                    // const fileName = './rec/'.concat(Date.now().toString(), '-').concat(userId, '.wav')
-
-                    // const arr1 = new Float32Array(leftConcatenatedBuffer.buffer);
-                    // const arr2 = new Float32Array(rightConcatenatedBuffer.buffer);
-                    // const data = {
-                    //     sampleRate: 4800,
-                    //     channels: 2,
-                    //     channelData: [arr1, arr2]
-                    // }
-
-                    // WavEncoder.encode(data).then((buffer) => {
-                    //     writeFileSync(fileName, Buffer.from(buffer), { encoding: "binary" })
-                    // })
-                })
-
-                //TODO: 音声を取得して、WAV形式に変換する
-                //TODO: WAV形式の音声をWhisperAPIに送信する
-            });
+            message.channel.send('録音を開始します')
+            voiceRecorder.startRecording(connection);
         } else {
             message.channel.send("ボイスチャンネルに接続してください");
         }
@@ -139,6 +69,23 @@ client.on('messageCreate', async (message: Message) => {
             const voiceChannel = message.member.voice.channel;
             const connection = voiceConnections.get(voiceChannel.guild.id);
             if (connection) {
+
+                const fileName = './rec/'.concat(Date.now().toString(), '-').concat(message.member.user.id, '.mp3')
+                const out = createWriteStream(fileName)
+
+                await voiceRecorder.getRecordedVoice(out, message.member.guild.id, "single", 5);
+
+                voiceRecorder.stopRecording(connection);
+
+                // message.channel.send({
+                //     content: "This is your voice",
+                //     files: [{
+                //         attachment: out,
+                //         contentType: "audio/mp3",
+                //         name: "output.mp3"
+                //     }]
+                // })
+
                 connection.disconnect();
                 voiceConnections.delete(voiceChannel.guild.id);
                 message.channel.send(`${voiceChannel.name}から切断しました`);
@@ -151,14 +98,6 @@ client.on('messageCreate', async (message: Message) => {
 
 })
 
-async function decodeOpus(opusStream: Buffer): Promise<Buffer> {
-    //OpusからPCMへ変換をする
-    return new Promise((resolve, reject) => {
-        const opusDecoder = new OpusEncoder(48000, 2);
-        const pcmData = opusDecoder.decode(opusStream);
-        resolve(pcmData);
-    });
-}
 
 //ボット作成時のトークンでDiscordと接続
 client.login(process.env.TOKEN)
