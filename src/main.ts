11d6c33caab86410c5dbb358282bec5fc787f53d
hirotask <hirotaskhashi@gmail.com>
2023-07-11 23:37:50 +0900

    [update]: コマンドの追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 //必要なパッケージをインポートする
-import { GatewayIntentBits, Client, Partials, Events, EmbedBuilder } from 'discord.js'
-import { joinVoiceChannel } from '@discordjs/voice'
+import { GatewayIntentBits, Client, Partials, Events, EmbedBuilder, Message, Snowflake } from 'discord.js'
 import dotenv from 'dotenv'
 import { SpeechEvents, addSpeechEvent, resolveSpeechWithGoogleSpeechV2 } from 'discord-speech-recognition'
+import { VoiceConnection, joinVoiceChannel } from '@discordjs/voice'
 
 //.envファイルを読み込む
 dotenv.config()
@@ -20,6 +20,9 @@ const client = new Client({
     partials: [Partials.Message, Partials.Channel],
 })
 
+const connect_commands = ["議事録取って", "議事録開始", "!start"];
+const stop_commands = ["議事録とめて", "議事録終了", "!stop"];
+
 //Botがきちんと起動したか確認
 client.once('ready', () => {
     console.log('Ready!')
@@ -35,19 +38,44 @@ addSpeechEvent(client, {
     minimalVoiceMessageDuration: 1,
 });
 
-client.on(Events.MessageCreate, (msg) => {
-    const voiceChannel = msg.member?.voice.channel;
-    if (voiceChannel) {
-        joinVoiceChannel({
-            channelId: voiceChannel.id,
-            guildId: voiceChannel.guild.id,
-            adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-            selfDeaf: false,
-        });
+let voiceConnections = new Map<Snowflake, VoiceConnection>();
 
+client.on(Events.MessageCreate, (message: Message) => {
+    if (message.author.bot) return
 
+    if (connect_commands.includes(message.content)) {
+        if (message.member && message.member?.voice.channel) {
+            const voiceChannel = message.member.voice.channel;
+            const connection = joinVoiceChannel({
+                channelId: voiceChannel.id,
+                guildId: voiceChannel.guild.id,
+                adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+                selfDeaf: false,
+            });
+
+            voiceConnections.set(voiceChannel.guild.id, connection);
+
+            message.channel.send("文字起こしを開始します")
+
+        } else {
+            message.channel.send("ボイスチャンネルに接続してください")
+        }
+    } else if (stop_commands.includes(message.content)) {
+        if (message.member && message.member.voice.channel) {
+            const voiceChannel = message.member.voice.channel;
+            const connection = voiceConnections.get(voiceChannel.guild.id)
+
+            if (connection) {
+                message.channel.send("文字起こしを終了します")
+                connection.disconnect()
+                voiceConnections.delete(voiceChannel.guild.id)
+            }
+        } else {
+            message.channel.send("BOICHANはボイスチャンネルに接続していません")
+        }
     }
-});
+})
+
 
 client.on(SpeechEvents.speech, (msg) => {
     // If bot didn't recognize speech, content will be empty
